17b41ae5543c135f2b5bce19804ddcedb7c0a3d9
Eliya <eliysamaea@gmail.com>
2025-03-31 21:49:03 +0300

    all foundation for chats and messages is in comments // use when chat in backend is fixed

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -1,16 +1,51 @@
 import { useAuth } from "../context/AuthContext";
-import { useState } from "react";
+import { useState } from "react"; // need also useEffect
 import ChatWindow from "../components/chat/ChatWindow";
 // import ChatList from "../components/chat/ChatList";
 import logo from "../assets/logo.png";
+// import { getMessages, createNewMessage } from "../api/message";
 
 function AppContainer() {
   const { currentUser, logout } = useAuth();
   const [messages, setMessages] = useState([]);
+  // const [selectedChatId, setSelectedChatId] = useState(null);
+
+  // const userId = currentUser?.data?.user?._id;
+  // const userEmail = currentUser?.data?.user?.email || currentUser.email;
+
+  // useEffect(() => {
+  //   if (userId && selectedChatId) {
+  //     getMessages(selectedChatId, userId)
+  //       .then(setMessages)
+  //       .catch((err) => console.error("Error loading messages:", err));
+  //   }
+  // }, [userId, selectedChatId]);
+
+  // const handleSend = async (text) => {
+  //   if (!userId || !selectedChatId) return;
+
+  //   try {
+  //     const newMsg = await createNewMessage({
+  //       userId,
+  //       chatId: selectedChatId,
+  //       sender: userEmail,
+  //       text,
+  //     });
+
+  //     setMessages((prev) => [...prev, newMsg]);
+  //   } catch (err) {
+  //     console.error("Error sending message:", err);
+  //   }
+  // };
+
+  // const handleChatSelect = (chatId) => {
+  //   setSelectedChatId(chatId);
+  //   setMessages([]); // Clear previous messages
+  // };
 
   const handleSend = (text) => {
     const newMessage = {
-      sender: currentUser.email,
+      sender: currentUser.data.user.email,
       text,
     };
     setMessages((prev) => [...prev, newMessage]);
@@ -24,7 +59,6 @@ function AppContainer() {
             Hello {currentUser.data.user.first_name} !
           </span>
           <img src={logo} alt="Logo" className="app-logo" />
-          {/* <h1 className="app-title">SmartChat</h1> */}
           <div className="user-info">
             <button className="logout-btn" onClick={logout}>
               Logout
@@ -33,7 +67,12 @@ function AppContainer() {
         </div>
       </header>
       <main className="app-main">
-        <div className="sidebar-chat-list">{/* <ChatList /> */}</div>
+        <div className="sidebar-chat-list">
+          {/* <ChatList
+            onSelectChat={handleChatSelect}
+            selectedChatId={selectedChatId}
+          /> */}
+        </div>
         <div className="app-content">
           <ChatWindow
             messages={messages}
