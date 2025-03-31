e7ba0a6086fd9ea2ab9bc7aa170aa9d6b4d9a1b7
Eliya <eliysamaea@gmail.com>
2025-03-31 16:22:55 +0300

    change the logo apeareance in landing page

--- src/containers/AuthContainer.jsx
+++ src/containers/AuthContainer.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import SignInForm from "../components/auth/SignInForm";
 import SignUpForm from "../components/auth/SignUpForm";
 import { useAuth } from "../context/AuthContext";
-import logo from "../assets/logo.jpg";
+import logo from "../assets/logo.png";
 
 function AuthContainer() {
   const [isFlipped, setIsFlipped] = useState(false);
@@ -31,10 +31,12 @@ function AuthContainer() {
 
   return (
     <div className="container">
+      {/* <h1>Welcome To SmartChat !</h1> */}
+      <img src={logo} alt="Logo" className="welcome-logo" />
+      {/* <h3>Please sign-in or sign-up</h3> */}
       <div className={`card ${isFlipped ? "flipped" : ""}`}>
         <div className="card-inner">
           <div className="card-front">
-            <img src={logo} alt="Logo" className="logo" />
             <SignInForm
               handleFlip={handleFlip}
               onSubmit={handleLogin}
@@ -42,8 +44,6 @@ function AuthContainer() {
             />
           </div>
           <div className="card-back">
-            <img src={logo} alt="Logo" className="logo" />
-
             <SignUpForm
               handleFlip={handleFlip}
               onSubmit={handleSignup}
