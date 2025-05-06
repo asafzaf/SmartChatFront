0d7f661457fdacc54a7f7cdb812a9b2a7e0c90c3
Eliya <eliysamaea@gmail.com>
2025-05-06 13:08:02 +0300

    adding a chat deleting

--- src/api/chat.js
+++ src/api/chat.js
@@ -5,7 +5,11 @@ const apiUrl = basicConfig.apiUrl;
 
 export const createNewChat = async (userId, socketId, prompt) => {
   try {
-    const response = await axios.post(`${apiUrl}/api/chat`, { userId, socketId, prompt });
+    const response = await axios.post(`${apiUrl}/api/chat`, {
+      userId,
+      socketId,
+      prompt,
+    });
     return response.data;
   } catch (error) {
     console.error("Error creating new chat:", error);
@@ -20,5 +24,17 @@ export const getChatList = async (userId) => {
   } catch (error) {
     console.error("Error fetching chat history:", error);
     throw error;
+  }
+};
+
+export const deleteChat = async (chatId) => {
+  try {
+    console.log("Enter chat deleting");
+    const res = await axios.delete(`${apiUrl}/api/chat/${chatId}`);
+    console.log("deleting chat:", res.data);
+    return res.data;
+  } catch (error) {
+    console.error("Error deleting chat:", error);
+    return { error };
   }
 };
