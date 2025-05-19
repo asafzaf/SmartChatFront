4643fbeda5f4d4157a8dd8fa59e9941efbb2747f
Eliya <eliysamaea@gmail.com>
2025-05-19 18:24:42 +0300

    add function to update user

--- src/api/user.js
+++ src/api/user.js
@@ -1,15 +1,25 @@
-// user.js
 import axios from "axios";
 
-export const updateUserPreferences = async (prefs, token) => {
-  const apiUrl = import.meta.env.VITE_API_URL;
+// export const updateUserPreferences = async (prefs, token) => {
+//   const apiUrl = import.meta.env.VITE_API_URL;
 
-  const response = await axios.put(`${apiUrl}/api/user/preferences`, prefs, {
-    headers: {
-      "Content-Type": "application/json", 
-      Authorization: `Bearer ${token || ""}`, 
-    },
-  });
+//   const response = await axios.put(`${apiUrl}/api/user/preferences`, prefs, {
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${token || ""}`,
+//     },
+//   });
 
-  return response.data;
+//   return response.data;
+// };
+
+export const updateUser = async (data, userId) => {
+  console.log("Updating user preferences:", data, userId);
+  try {
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const res = await axios.put(`${apiUrl}/api/user/${userId}`, data);
+    return res.data;
+  } catch (error) {
+    return { error };
+  }
 };
