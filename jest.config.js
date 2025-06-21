7994eb7f73575525e85a0a6b5024b99277bbd4c7
Nave Maymon <nave.may@gmail.com>
2025-06-21 15:35:28 +0300

    jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+// jest.config.js
+
+export default {
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["js", "jsx"],
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!react-markdown|remark-gfm)"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(png|jpg|jpeg|gif|svg|webp|mp4|mp3|woff2?|eot|ttf|otf)$": "<rootDir>/fileMock.js",
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+};
+
+
